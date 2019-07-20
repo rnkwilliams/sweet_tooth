0c5594cea2d39b77838c25e96e0b13823ca8d71c
Ranika Williams <rnkprice07@gmail.com>
2019-07-20 23:49:36 +0000

    Add require contents to environment file and methods to clifile

--- lib/sweet_tooth.rb
+++ lib/sweet_tooth.rb
@@ -1,4 +1,11 @@
+require 'pry'
+
 require "sweet_tooth/version"
+
+require_relative "sweet_tooth/version"
+require_relative "sweet_tooth/cli"
+require_relative "sweet_tooth/api"
+require_relative "sweet_tooth/shop"
 
 module SweetTooth
   class Error < StandardError; end
---
+++ lib/sweet_tooth/cli.rb
@@ -0,0 +1,11 @@
+class SweetTooth::CLI
+  
+  def call
+    puts "Welcome to Sweet Tooth!\n"
+    list_shops
+    menu
+    goodbye
+  end
+  
+  
+end
