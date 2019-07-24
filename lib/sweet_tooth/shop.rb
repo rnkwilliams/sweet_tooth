90f2fc52c2681cc6a524468f6815382b190dffbd
Ranika Williams <rnkprice07@gmail.com>
2019-07-24 02:03:26 +0000

    Add methods to shop.rb

--- lib/sweet_tooth/shop.rb
+++ lib/sweet_tooth/shop.rb
@@ -1,20 +1,35 @@
 class SweetTooth::Shop
   attr_accessor :name, :dessert_type, :price_range, :location, :contact
   
+  
   #@@all = []
   
   
-   def self.list_of_shops
-     
-     self.new_from_collection
-   end
+  #def initialize(attrs)
    
-   def self.new_from_collection
-    shops = []
-      
-     
+  #end
   
-   end
+   #def self.list_of_shops
+     
+     #self.new_from_collection
+   #end
+   
+   #def self.new_from_collection
+    #shops = []
+   #end
+   
+   def self.new_from_collection(shops)
+    shops.each do |attrs|
+        new(attrs)
+        #binding.pry
+    end 
+  end
+
+
+  def self.get_shops
+    SweetTooth::API.get_shops
+    all
+  end
   
   #def self.all
     #get_shops if @@all == []
@@ -23,9 +38,10 @@ class SweetTooth::Shop
   
   #def save
     #@@all << self
-  #end
+ #end
 end
 
+#SweetTooth::Shop.new
      #shop_1 = self.new
      #shop_1.name = "Sprinkles"
      #shop_1.dessert_type = "Cupcakes, Bakery"
