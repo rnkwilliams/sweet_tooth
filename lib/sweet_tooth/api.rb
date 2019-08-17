6ebd55e0deb8019bf225ae08d45485fd8edc07cd
Ranika Williams <rnkprice07@gmail.com>
2019-08-17 01:26:34 +0000

    Add changes to API

--- lib/sweet_tooth/api.rb
+++ lib/sweet_tooth/api.rb
@@ -2,7 +2,7 @@ class SweetTooth::API
 
   def self.get_shops
     resp = HTTParty.get("https://api.yelp.com/v3/businesses/search?term=desserts&limit=10&location=Dallas&sort_by=rating", {
-      headers: {"Authorization" => "Bearer #{ENV['API_KEY']}"},
+      headers: {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"},
     })
 
     shops = resp["businesses"]
