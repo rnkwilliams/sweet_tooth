520065f773663f3534e566414eac750341cff147
Ranika Williams <rnkprice07@gmail.com>
2019-08-17 00:43:51 +0000

    Add changes to api file

--- lib/sweet_tooth/api.rb
+++ lib/sweet_tooth/api.rb
@@ -1,11 +1,12 @@
 class SweetTooth::API
-  
+
   def self.get_shops
     resp = HTTParty.get("https://api.yelp.com/v3/businesses/search?term=desserts&limit=10&location=Dallas&sort_by=rating", {
       headers: {"Authorization" => "Bearer #{ENV['YELP_API_KEY']}"},
     })
-    
+
     shops = resp["businesses"]
+    binding.pry
     SweetTooth::Shop.new_from_collection(shops)
   end
 end
--- lib/sweet_tooth/shop.rb
+++ lib/sweet_tooth/shop.rb
@@ -1,8 +1,8 @@
 class SweetTooth::Shop
   attr_accessor :name, :dessert_type, :price_range, :location, :contact
-  
+
   @@all = []
-  
+
   def initialize(attrs)
     @name = attrs["name"]
     @dessert_type = attrs["categories"][0]["title"] unless attrs["categories"][0].nil?
@@ -11,32 +11,28 @@ class SweetTooth::Shop
     @contact = attrs["display_phone"]
     save
   end
-   
+
   def self.new_from_collection(shops)
     shops.each do |attrs|
         new(attrs)
    end
   end
-  
+
   def self.get_shops
     SweetTooth::API.get_shops
     all
   end
-  
+
   def self.find_all_by_name(string)
    all.select { |shop| shop.name.include?(string)}
-  end 
-  
+  end
+
   def self.all
     get_shops if @@all == []
     @@all
   end
-  
+
   def save
     @@all << self
   end
 end
-
-
-
-    
